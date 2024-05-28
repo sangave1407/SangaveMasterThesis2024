cb5d364bf6a2cba1d71ff08b66edf5f24b4cd7a6
Ben Bausch <45594477+BenBausch@users.noreply.github.com>
2024-05-28 15:09:11 +0200

    Unet (#1)
    * wip
    
    * removed old file

---
+++ src/datasets/dataset.py
@@ -0,0 +1,72 @@
+import os
+import h5py
+import random
+import torch
+import random
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+
+
+class SleepDataset(Dataset):
+    def __init__(self, data_path, split_file, channels, labels, window_size, transform=None):
+        self.hdf5_files = []
+        with open(split_file, "r") as f:
+            for row in f.readlines():
+                self.hdf5_files.append(os.path.join(
+                    data_path, row.strip() + ".hdf5"))
+        self.transform = transform
+        self.channels = channels
+        self.labels = labels
+        self.window_size = window_size
+
+    def __len__(self):
+        return len(self.hdf5_files)
+
+    def __getitem__(self, idx):
+        patient_path = self.hdf5_files[idx]
+        with h5py.File(patient_path, 'r') as file:
+            # Collect the individual channels
+            data = []
+            for c in self.channels:
+                data.append(torch.tensor(file[c][:]))
+            # Combine the channels into a single tensor
+            data = torch.stack(data, dim=0)
+
+            # Collect the labels
+            patient_labels = []
+            for l in self.labels:
+                patient_labels.append(torch.tensor(file[l][:]))
+            # Combine the labels into a single tensor
+            patient_labels = torch.stack(patient_labels, dim=0)
+
+            # Randomly select a window of size self.window_size
+            start = random.randint(0, data.shape[1] - self.window_size)
+            data = data[:, start: start + self.window_size]
+            patient_labels = patient_labels[:, start: start + self.window_size]
+
+            # Remove unnecessary dimensions
+            data = torch.squeeze(data, dim=2)
+            patient_labels = torch.squeeze(patient_labels, dim=2)
+            patient_labels = patient_labels.to(torch.float32)
+
+            # Apply the transformation function
+            if self.transform:
+                try:
+                    data = self.transform(data)
+                except Exception as e:
+                    print(
+                        f"Error applying transform to sample at index {idx}: {e}")
+
+            return data, patient_labels
+
+
+if __name__ == "__main__":
+    data_path = "data/hdf5"
+    split_file = "data/splits/split1/train.txt"
+    channels = ["x2", "x3"]
+    labels = ["y", "sleep_label"]
+    window_size = 10
+    ds = SleepDataset(data_path, split_file, channels, labels,
+                      window_size, transform=None)
+    data, labels = ds.__getitem__(0)
+    print(data.dtype, labels.dtype)
---
+++ src/train.py
@@ -0,0 +1,54 @@
+import torch
+
+
+class Trainer():
+    def __init__(self, model, optimizer, criterion, device, result_path):
+        self.model = model
+        self.optimizer = optimizer
+        self.criterion = criterion
+        self.device = device
+        self.last_validation_loss = float("inf")
+        self.result_path = result_path
+
+    def fit(self, train_loader, val_loader, num_epochs):
+        for epoch in range(num_epochs):
+            self.train(train_loader, epoch)
+            self.validate(val_loader, epoch)
+
+    def train(self, train_loader, epoch):
+        self.model.train()
+        running_loss = 0.0
+        for idx, (data, labels) in enumerate(train_loader):
+            data, labels = data.to(self.device), labels.to(self.device)
+            self.optimizer.zero_grad()
+            outputs = self.model(data)
+            loss = self.criterion(outputs, labels)
+            loss.backward()
+            self.optimizer.step()
+            running_loss += loss.item()
+            if idx % 1 == 1:
+                print("Epoch:", epoch + 1, "Batch:",
+                      idx + 1, "Loss:", running_loss / 100)
+                running_loss = 0.0
+
+    def validate(self, val_loader, epoch):
+        self.model.eval()
+        running_loss = 0.0
+        with torch.no_grad():
+            for idx, (data, labels) in enumerate(val_loader):
+                data, labels = data.to(self.device), labels.to(self.device)
+                outputs = self.model(data)
+                loss = self.criterion(outputs, labels)
+                running_loss += loss.item()
+        val_loss = running_loss / len(val_loader)
+        print(f"Validation loss: {val_loss}")
+
+        if val_loss < self.last_validation_loss:
+            self.last_validation_loss = val_loss
+            path = f"{self.result_path}/model_{epoch}.pth"
+            print("Validation loss decreased. Saving model.")
+            self.save(path)
+
+    def save(self, path):
+        torch.save(self.model.state_dict(), path)
+        print(f"Model saved to {path}")
